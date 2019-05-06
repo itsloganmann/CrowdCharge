e3d39fdae7bced40eba272ec000592f7d5068a97
Eric Huang <ericwhhuang@gmail.com>
2019-05-06 12:56:56 -0700

    Implemented sign out

--- js/firebase_init.js
+++ js/firebase_init.js
@@ -13,18 +13,14 @@
   app_firebase = firebase; 
 })();
 
+// Firebase Authentication
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 var uiConfig = {
   callbacks: {
     signInSuccessWithAuthResult: function(authResult, redirectUrl) {
-      // User successfully signed in.
-      // Return type determines whether we continue the redirect automatically
-      // or whether we leave that to developer to handle.
       return true;
     },
     uiShown: function() {
-      // The widget is rendered.
-      // Hide the loader.
       $('#firebaseui-auth-container').css({
         "display" : "none",
         "position" : "fixed",
@@ -34,18 +30,14 @@ var uiConfig = {
       });
     }
   },
-  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
-  signInSuccessUrl: '<url-to-redirect-to-on-success>',
+  signInSuccessUrl: 'index.html',
   signInOptions: [{
-    // Leave the lines as is for the providers you want to offer your users.
     provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
     requireDisplayName: true
     }
   ],
-  // Terms of service url.
   tosUrl: '<your-tos-url>',
-  // Privacy policy url.
   privacyPolicyUrl: '<your-privacy-policy-url>'
 };
 
--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-    $("#auth-button").on("click", function(){
-        $('#firebaseui-auth-container').css("display", "block");
+    firebase.auth().onAuthStateChanged(function (user) {
+        if (user) {
+            $("#auth-button").on("click", function(){
+                firebase.auth().signOut().then(function () {
+                    location.replace("index.html");
+                });
+            });
+            console.log(user.uid);
+        } else {
+            $("#auth-button").on("click", function(){
+                $('#firebaseui-auth-container').css("display", "block");
+            });
+        }
     });
 });
