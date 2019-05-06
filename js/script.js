8b4165b480a19c469543dddeae428b3233d2a2f1
Eric Huang <ericwhhuang@gmail.com>
2019-05-06 14:22:52 -0700

    Added login semi-transparent overlay

--- js/script.js
+++ js/script.js
@@ -10,8 +10,14 @@ $(document).ready(function() {
         } else {
             $("#login-button").css("display", "block");
             $("#login-button").on("click", () => {
+                $('#overlay').remove();
+                $("body").prepend("<div id='overlay'></div>")
                 $('#firebaseui-auth-container').css("display", "block");
             });
         }
+    });
+    $("body").on("click", "#overlay", () => {
+        $('#overlay').remove();
+        $('#firebaseui-auth-container').css("display", "none");
     });
 });
