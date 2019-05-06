2647a8241bfc9684aafa91c74c5cdf01d5f2fd91
Eric Huang <ericwhhuang@gmail.com>
2019-05-06 12:41:28 -0700

    Login functionality and popup implemented

--- js/firebase_init.js
+++ js/firebase_init.js
@@ -1,6 +1,5 @@
 // Code snippet from Firebase for setting up web app.
 (function(){  
-
   var config = {
     apiKey: "AIzaSyBXOtHAyVkN2mZraH3sBpBvkCpgdKpmDyI",
     authDomain: "zapshare-1e3ba.firebaseapp.com",
@@ -13,3 +12,41 @@
 
   app_firebase = firebase; 
 })();
+
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+var uiConfig = {
+  callbacks: {
+    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+      // User successfully signed in.
+      // Return type determines whether we continue the redirect automatically
+      // or whether we leave that to developer to handle.
+      return true;
+    },
+    uiShown: function() {
+      // The widget is rendered.
+      // Hide the loader.
+      $('#firebaseui-auth-container').css({
+        "display" : "none",
+        "position" : "fixed",
+        "left" : "0",
+        "right" : "0",
+        "z-index" : "2"
+      });
+    }
+  },
+  // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+  signInFlow: 'popup',
+  signInSuccessUrl: '<url-to-redirect-to-on-success>',
+  signInOptions: [{
+    // Leave the lines as is for the providers you want to offer your users.
+    provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    requireDisplayName: true
+    }
+  ],
+  // Terms of service url.
+  tosUrl: '<your-tos-url>',
+  // Privacy policy url.
+  privacyPolicyUrl: '<your-privacy-policy-url>'
+};
+
+ui.start('#firebaseui-auth-container', uiConfig);
---
+++ js/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $("#auth-button").on("click", function(){
+        $('#firebaseui-auth-container').css("display", "block");
+    });
+});
