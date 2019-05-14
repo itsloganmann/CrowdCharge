ccba6b13971d302f19b090a4347d405e0f3dcc66
Edwin-Pau <pau.edwin@live.ca>
2019-05-14 12:18:20 -0700

    Changed to cloud db

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose')
+const atlas = 'mongodb+srv://zapshare:comp2930@zapshare-api-wylcv.mongodb.net/test?retryWrites=true'
+const local = 'mongodb://127.0.0.1:27017/zapshare-api'
 
 // Configures mongoose. Connects to the database.
-mongoose.connect('mongodb://127.0.0.1:27017/zapshare-api', {
+mongoose.connect(atlas, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
