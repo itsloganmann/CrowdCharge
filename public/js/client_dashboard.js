54e90a24bbe9a65aec928ac53bb579d913355872
Louis Lu <j95159@gmail.com>
2019-05-14 22:37:13 -0700

    fixed client_dashboard

--- public/js/client_dashboard.js
+++ public/js/client_dashboard.js
@@ -1,84 +1,114 @@
 console.log("js file loaded successfullly");
-function grabDate(time){
+
+function grabDate(time) {
 	let result = "";
-	let c = "";
+
 	let count = 0;
-	while(c!="T"){
-		c = time[count];
+	let c = "";
+	while (c != "T") {
 		result += c;
-		count++;
+		c = time[count++];
 	}
 	return result;
 };
-//booking tab click; build elements for booking details
+function grabTime(time) {
+	let result = "";
+	let c = "";
+	let count = 0;
+	while (c != "T") {
+		c = time[count];
+		count++;
+	}
+	for (i = count; i < time.length; i++) {
+		result += time[i];
+	}
+	return result;
+};
+function fetchBooking(url, status) {
+	let data = {};
+	let contentString = "";
+	fetch(url)
+		.then((res) => {
+			return res.json()
+		})
+		.then((db) => {
+			data = JSON.parse(JSON.stringify(db))
+		});
+	//////////////////////////////////////temp data
+	data = {
+		startTime: "2019-01-01T00:00:00",
+		endTime: "2019-01-01T23:00:00",
+		cost: "$15.00",
+		address: "12345 MyHome St.",
+		city: "Vancouver, BC"
+	}
+	/////////////////////////////////////To BE REMOVE
+	if (status == "pending") {
+		contentString = "<div class= 'col-10 well'>"
+		+ "<p class='date'>" + grabDate(data.startTime) + "</p>"
+		+ "<p class='cost'>" + data.cost + "</p>"
+	    + "<p class='time'>" + grabTime(data.startTime) + "-" + grabTime(data.endTime) + "</p>"
+		+ "<p class='city'>" + data.city + "</p>"
+		+ "</div>";
+	}
+	else {
+		contentString = "<div class= 'col-10 well'>"
+			+ "<p class='date'>" + grabDate(data.startTime) + "</p>"
+			+ "<div style='color: red; float: right'><p class='cost'>" + data.cost + "</p>";
+		if (status == "paid") {
+			contentString += "<p class='green'>" + status + "</p></div>"
+		}
+		else if (status == "unpaid") {
+			contentString += "<p class='red'>" + status + "</p></div>"
+		}
+
+		contentString += "<p class='time'>" + grabTime(data.startTime) + "-" + grabTime(data.endTime) + "</p>"
+			+ "<p class='address'>" + data.address + "</p>"
+			+ "<p class='city'>" + data.city + "</p>"
+			+ "</div>";
+	}
+	return contentString;
+};
+
+//tab's eventListener
 $("#bookings").click(function (event) {
 	$("#bookings").css({ "color": "#F05A29" });
 	$("#payment").css({ "color": "black" });
 	$("#reviews").css({ "color": "black" });
-
 	$("#subContent").html("");
-
 
 	/*
 	CONFIRMED BOOKING
 	*/
-	//container hold all confirmed bookings for user
 	var confirmContainer = $("<div class='col-12'></div>");
 	var bookingHeading1 = $("<h3 class='col-10 content-margin h1-font-size' id='bookingHeading1'><b>Confirmed Bookings</b></h3>");
 	var bookingSubHeading1 = $("<h6 class='col-10 content-margin h2-font-size' id='bookingSubHeading1'> "
 		+ "These bookings have been confirmed by the host and are ready to go!</h6>");
-	var confirmedBookingData;
-	//fetch
+
+	//URL param 
 	const confirmedBookingReq = {
 		user_id: "",
 		booking_type: "paid"
 	}
 	const confirmedBookingURL = "/bookings?" + $.param(confirmedBookingReq);
-	fetch(confirmedBookingURL).then((res)=> {return res.json()
-	}).then((db)=> {
-		const data = JSON.parse(db);
-		//const data = db;
-		confirmedBookingData = $("<div class= 'col-10 well' id='confirmedBookingData>"
-			+ "<p id='cb-date'>" + data.bookingDate + "</p>"
-			+ "<p id='cb-cost'>" + data.cost + "</p>"
-			+ "<p id='cb-time'>" + data.startTime + "-" + data.endTime + "</p>"
-			+ "<p id='cb-address'>" + data.address + "</p>"
-			+ "<p id='cb-city'>" + data.city + "</p>"
-			+ "</div>");})
+	let cBData = fetchBooking(confirmedBookingURL, "paid");
+	var confirmedBookingData = $(cBData);
 
 	/*
 	PENDING BOOKING
 	*/
-	//container hold all pending bookings for user
 	var pendingContainer = $("<div class='col-12'></div>");
 	var bookingHeading2 = $("<h3 class='col-10  content-margin h1-font-size' id= 'bookingHeading2'><b>Pending Bookings</b></h3>");
 	var bookingSubHeading2 = $("<h6 class='col-10 content-margin h2-font-size' id='bookingSubHeading2'>"
 		+ "These bookings have not been confirmed by the host yet, we’ll notify you when they do!</h6>");
-	var pendingBookingData;
+	//URL param
 	const pendingBookingReq = {
 		user_id: "",
 		booking_type: "pending"
 	}
-	// fetch('/client/paybooking', {
-	// 	method: 'POST',
-	// 	header: {
-	// 		'content-type': 'application/json',
-	// 	},
-	// 	body: {
-	// 		data: JSON.stringify(pendingBookingReq)
-	// 	}
-	// }).then((res) => {
-	// 	return res.json()
-	// }).then((db) => {
-	// 	const data = JSON.parse(db);
-	// 	pendingBookingData = $("<div class='col-10 well' id='pendingBookingData'>"
-	// 		+ "<p id='pb-date'>" + data.date + "</p>"
-	// 		+ "<p id='pb-cost'>" + data.cost + "</p>"
-	// 		+ "<p id='pb-time'>" + data.startTime + "-" + data.endTime + "</p>"
-	// 		+ "<p id='pb-address'>" + data.address + "</p>"
-	// 		+ "<p id='pb-city'>" + data.city + "</p>"
-	// 		+ "</div>");
-	// });
+	const pendingBookingURL = "/bookings?" + $.param(pendingBookingReq);
+	var pbData = fetchBooking(pendingBookingURL, "pending");
+	var pendingBookingData = $(pbData);
 
 	//appending
 	confirmContainer.append(bookingHeading1);
@@ -105,29 +135,14 @@ $("#payment").click(function (event) {
 	var paymentHeading1 = $("<h3 class='col-10 content-margin h1-font-size' id='paymentHeading1'><b>Payment</b></h3>");
 	var paymentSubHeading1 = $("<h6 class='col-10 content-margin h2-font-size' id='paymentSubHeading1'>" +
 		"These bookings are unpaid for. Pay before the booking date!</h6>");
-	var paymentData;
 
 	const unpaidBookingReq = {
 		user_id: "",
 		booking_type: "unpaid"
 	}
-	// fetch('/client/paybooking', {
-	// 	method: 'POST',
-	// 	header: {
-	// 		'content-type': 'application/json',
-	// 	},
-	// 	body: JSON.stringify(unpaidBookingReq)
-	// }).then((res) => { return res.json() 
-	// }).then((db) => {
-	// 		const data = JSON.parse(db);
-	// 		paymentData = $("<div class='col-10 well' id='paymentData'>"
-	// 			+ "<p id='ub-date'>" + data.date + "</p>"
-	// 			+ "<p id='ub-cost'>" + data.cost + "</p>"
-	// 			+ "<p id='ub-time'>" + data.startTime + "-" + data.endTime + "</p>"
-	// 			+ "<p id='ub-address'>" + data.address + "</p>"
-	// 			+ "<p id='ub-city'>" + data.city + "</p>"
-	// 			+ "</div>");
-	// 	});
+	const unpaidBookingURL = "/bookings?" + $.param(unpaidBookingReq);
+	const ubData = fetchBooking(unpaidBookingURL, "unpaid");
+	var paymentData = $(ubData);
 	//appending
 	paymentContainer.append(paymentHeading1);
 	paymentContainer.append(paymentSubHeading1);
@@ -154,23 +169,20 @@ $("#reviews").click(function (event) {
 	const reviewReq = {
 		user_id: "",
 	}
-	// fetch('/client/paybooking', {
-	// 	method: 'POST',
-	// 	header: {
-	// 		'content-type': 'application/json',
-	// 	},
-	// 	body: {
-	// 		data: JSON.stringify(reviewReq)
-	// 	}
-	// }).then((res) => { return res.json() 
-	// }).then((db) => {
-	// 		const data = JSON.parse(db);
-	// 		reviewsData = $("<div class='col-10 well' id='reviewsData'>"
-	// 			+ "<p id='rv-reviewer'>" + data.user + "</p>"
-	// 			+ "<p id='rv-rating'>" + data.rating + "</p>"
-	// 			+ "<p id='rv-comment'>" + data.comment + "</p>"
-	// 			+ "</div>");
-	// 	});
+	const reviewURL = "/bookings?" + $.param(reviewReq);
+	fetch(reviewURL)
+		.then((res) => {
+			return res.json();
+		})
+		.then((db) => {
+			let data = JSON.parse(JSON.stringify(db));
+			reviewsData = $("<div class='col-10 well' id='reviewsData'>"
+				+ "<p id='rv-reviewer'>" + data.user + "</p>"
+				+ "<p id='rv-rating'>" + data.rating + "</p>"
+				+ "<p id='rv-comment'>" + data.comment + "</p>"
+				+ "</div>");
+		});
+
 	//appending
 	reviewContainer.append(reviewHeading1);
 	reviewContainer.append(reviewSubHeading1);
