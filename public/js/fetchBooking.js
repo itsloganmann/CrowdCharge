c9b000e25f82fcf7aa271edef37b35094924ddac
Louis Lu <j95159@gmail.com>
2019-05-18 15:19:15 -0700

    client dashboard fetch now can grab correct data from database(array not just an object); host_dashboard refactor from bookings-tab to requests-tab:

--- public/js/fetchBooking.js
+++ public/js/fetchBooking.js
@@ -1,10 +1,10 @@
 function fetchBooking(url, status) {
 	const jwt = localStorage.getItem('jwt');
 	let data = {};
-	let contentString = "";
+	let contentStrings = [];
 	fetch(url, {
 		method: 'GET',
-		headers : {
+		headers: {
 			'content-type': 'application/json',
 			'Authorization': 'Bearer ' + jwt
 		}
@@ -13,10 +13,10 @@ function fetchBooking(url, status) {
 			return res.json()
 		})
 		.then((db) => {
-			data = JSON.parse(JSON.stringify(db))
-		});
+			data = db
+		}).catch(error => console.log(error));
 	//////////////////////////////////////temp data
-	data = {
+	data = [{
 		startTime: "2019-01-01T00:00:00Z",
 		endTime: "2019-01-01T23:00:00Z",
 		cost: "$15.00",
@@ -24,40 +24,50 @@ function fetchBooking(url, status) {
 		city: "Vancouver",
 		province: "BC",
 		host: "Louis"
-	}
+	},
+	{
+		startTime: "2019-01-01T00:00:00Z",
+		endTime: "2019-01-01T23:00:00Z",
+		cost: "$15.00",
+		address: "12345 MyHome St.",
+		city: "Vancouver",
+		province: "BC",
+		host: "Louis"
+	}]
 	/////////////////////////////////////To BE REMOVE
-	if (status == "pending") {
-		contentString = "<div class= 'col-11 tab-section-data row'><div class='card-panel col-md-5'>"
-			+ "<div class='right'><p class='cost'>" + data.cost + "</p></div>"
-			+ "<p class='date'>" + data.startTime.split("T")[0] + "</p>"
-			+ "<p class='time'>" + data.startTime.split("T")[1].split("Z")[0] + "-" + data.endTime.split("T")[1].split("Z")[0] + "</p>"
-			+ "<p class='city'>" + data.city + "</p>"
-			+ "</div></div>";
-	}
-	else if (status == "complete") {
-		console.log("Hey");
-		contentString = "<div class = 'col-10 well'>" + "<div class='right'><p class='cost'>" 
-		+ data.cost + "</p>" + "<p class = 'date'>" + data.startTime.split("T")[0] + "</p>" 
-		+ "<p class = 'address'>" + data.address + "</p>" + "<p class = 'city'>" + data.city 
-		+ "<p class = 'province'>" + data.province + "</p>" + "<p class = 'hostName'>" 
-		+ data.host + "</p>" + "</div>" + "</div>" 
-		console.log(contentString);
-	}
-	else {
-		contentString = "<div class= 'col-11 tab-section-data row'><div class='card-panel col-md-5'>"
-		+ "<div class='price-card-text-wrapper'><div class='price-card-text-lg'>" + data.cost + "</div>";
-		if (status == "paid") {
-			contentString += "<div class='price-card-text-sm'>" + status + "</div></div>";
+	for (i = 0; i < data.length; i++) {
+		if (status == "pending") {
+			contentStrings[i] = "<div class= 'col-11 tab-section-data row'><div class='card-panel col-md-5'>"
+				+ "<div class='right'><p class='cost'>" + data[i].cost + "</p></div>"
+				+ "<p class='date'>" + data[i].startTime.split("T")[0] + "</p>"
+				+ "<p class='time'>" + data[i].startTime.split("T")[1].split("Z")[0] + "-" + data[i].endTime.split("T")[1].split("Z")[0] + "</p>"
+				+ "<p class='city'>" + data[i].city + "</p>"
+				+ "</div></div>";
 		}
-		else if (status == "unpaid") {
-			contentString += "<div class='price-card-text-sm'>" + status + "</div></div>";
+		else if (status == "complete") {
+			contentStrings[i] = "<div class = 'col-11 tab-section-data row'><div class='card-panel col-md-5'>"
+				+ "<div class='right'><p class='cost'>" + data[i].cost + "</p></div>"
+				+ "<p class = 'date'>" + data[i].startTime.split("T")[0] + "</p>"
+				+ "<p class = 'address'>" + data[i].address + "</p>" + "<p class ='city'>" + data[i].city
+				+ "<p class = 'province'>" + data[i].province + "</p>" + "<p class='hostName'>"
+				+ data[i].host + "</p>" + "</div></div>"
 		}
-		console.log(data.startTime);
-		contentString += "<div class='card-text-lg'>" + data.startTime.split("T")[0] + "</div>"
-			+ "<div class='card-text-md'>" + data.startTime.split("T")[1] + "-" + data.endTime.split("T")[1] + "</div>"
-			+ "<div class='card-text-sm'>" + data.address + "</div>"
-			+ "<div class='card-text-sm'>" + data.city + "</div>"
-			+ "</div></div>";
-	}
-	return contentString;
+		else {
+			contentStrings[i] = "<div class= 'col-11 tab-section-data row'><div class='card-panel col-md-5'>"
+				+ "<div class='price-card-text-wrapper'><div class='price-card-text-lg'>" + data[i].cost + "</div>";
+			if (status == "paid") {
+				contentStrings[i] += "<div class='price-card-text-sm'>" + status + "</div></div>";
+			}
+			else if (status == "unpaid") {
+				contentStrings[i] += "<div class='price-card-text-sm'>" + status + "</div></div>";
+			}
+			contentStrings[i] += "<div class='card-text-lg'>" + data[i].startTime.split("T")[0] + "</div>"
+				+ "<div class='card-text-md'>" + data[i].startTime.split("T")[1] + "-" + data[i].endTime.split("T")[1] + "</div>"
+				+ "<div class='card-text-sm'>" + data[i].address + "</div>"
+				+ "<div class='card-text-sm'>" + data[i].city + "</div>"
+				+ "</div></div>";
+		}
+	};
+		return contentStrings;
+
 };
