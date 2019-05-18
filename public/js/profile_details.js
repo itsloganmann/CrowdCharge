94acc5ca662d8aadab982ea95f5cfe4317a0de5e
Alex Giasson <giasson.alexj@gmail.com>
2019-05-18 16:40:02 -0700

    more onload functionality

--- public/js/profile_details.js
+++ public/js/profile_details.js
@@ -1,27 +1,5 @@
 const jwt = localStorage.getItem('jwt');
 
-fetch('/users/me', {
-    method: 'GET',
-    headers: {
-        'content-type': 'application/json',
-        'Authorization': 'Bearer ' + jwt
-    }
-}).then((res) => {
-    return res.json()
-}).then(async (db) => {
-    $('#profile-name-input').val(db.name);
-    $('#profile-email-input').val(db.email);
-    $('#profile-phone-input').val(db.phone);
-
-    $('#profile-name-input').attr('readonly', 'true');
-    $('#profile-email-input').attr('readonly', 'true');
-    $('#profile-phone-input').attr('readonly', 'true');
-    $('#profile-currentpassword-input').attr('readonly', 'true');
-    $('#profile-newpassword-input').attr('readonly', 'true');
-    $('#profile-confirmpassword-input').attr('readonly', 'true');
-
-}).catch(error => console.log(error));
-
 // Changes tab colours and clears tab contents
 // Clearing done when switching tabs to allow for new data population
 $('.tab-button').on('click', (e) => {
@@ -30,9 +8,34 @@ $('.tab-button').on('click', (e) => {
 	$('#tab-content').children().remove();
 });
 
+// named function for default tab load and on click
 function defaultTab () {
-    var detailsContainer = createContentContainer('details-content', 'details-header', 'Profile Details', '', '');
 
+    // fetch user data
+    fetch('/users/me', {
+        method: 'GET',
+        headers: {
+            'content-type': 'application/json',
+            'Authorization': 'Bearer ' + jwt
+        }
+    }).then((res) => {
+        return res.json()
+    }).then(async (db) => {
+        $('#profile-name-input').val(db.name);
+        $('#profile-email-input').val(db.email);
+        $('#profile-phone-input').val(db.phone);
+
+        $('#profile-name-input').attr('readonly', 'true');
+        $('#profile-email-input').attr('readonly', 'true');
+        $('#profile-phone-input').attr('readonly', 'true');
+        $('#profile-currentpassword-input').attr('readonly', 'true');
+        $('#profile-newpassword-input').attr('readonly', 'true');
+        $('#profile-confirmpassword-input').attr('readonly', 'true');
+
+    }).catch(error => console.log(error));
+
+    // Rendering
+    var detailsContainer = createContentContainer('details-content', 'details-header', 'Profile Details', '', '');
     $(detailsContainer).append($(
         "<div id='profile-details' class='tab-section-content col-12'>" +
                 "<form class='col-11 tab-section-data row' id='profile-details-form'>" +
@@ -54,22 +57,75 @@ function defaultTab () {
                 "</form>" +
             "</div>"
     ));
-
     $('#tab-content').append(detailsContainer);
+
+    // Edit Button behavior
+    $('#edit-btn').click(function (event) {
+        event.preventDefault();
+        $('#profile-name-input').css({ 'background-color': 'white' });
+        $('#profile-email-input').css({ 'background-color': 'white' });
+        $('#profile-phone-input').css({ 'background-color': 'white' });
+    
+        $('#profile-name-input').removeAttr('readonly');
+        $('#profile-email-input').removeAttr('readonly');
+        $('#profile-phone-input').removeAttr('readonly');
+    
+        $('#edit-btn').css({ 'display': 'none' });
+        $('#save-btn').css({ 'display': 'block' });
+    });
+    
+    //Save button behavior
+    $('#save-btn').click((event) => {
+        event.preventDefault();
+    
+        $('#profile-name-input').css({ 'background-color': 'inherit' });
+        $('#profile-email-input').css({ 'background-color': 'inherit' });
+        $('#profile-phone-input').css({ 'background-color': 'inherit' });
+    
+        $('#profile-name-input').attr('readonly', 'true');
+        $('#profile-email-input').attr('readonly', 'true');
+        $('#profile-phone-input').attr('readonly', 'true');
+    
+        $('#save-btn').css({ 'display': 'none' });
+        $('#edit-btn').css({ 'display': 'block' });
+    
+    
+        //POST
+        var name = $('#profile-name-input').val();
+        var phone = $('#profile-phone-input').val();
+        var email = $('#profile-email-input').val();
+        const dataToSend = {
+            name: name,
+            phone: phone,
+            email: email,
+        }
+    
+        fetch('/users/me', {
+            method: 'PATCH',
+            body: JSON.stringify(dataToSend),
+            headers: {
+                'content-type': 'application/json',
+                'Authorization': 'Bearer ' + jwt
+            }
+        }).then(res => {
+            console.log(res)
+    
+        }).then((response) => {
+        }).catch(error => console.error('Error:', error));
+    });
 };
 
 window.onload = function() {
-    console.log("Onload");
     defaultTab();
     $('#details-tab').css({ 'color': '#F05A29' });
-
 };
 
+// detail tab event listener
 $('#details-tab').click(function (event) {
     defaultTab();
 })
 
-//tab's eventListener
+// password tab's eventListener
 $('#password-tab').click(function (event) {
 
     // Create a container
@@ -90,11 +146,17 @@ $('#password-tab').click(function (event) {
                 "<label id='profile-confirmpassword-label' class='form-label profile-label' for='profile-confirmpassword-input'>Confirm new password</label>" +
                 "<input type='text' name='confirmpassword' id='profile-confirmpassword-input' class='form-input profile-input' >" +
             "</div>" +
-            "<input type='submit' id='submit-btn' class='orange-button small-btn' value='Save Password'></button>" +
+            "<input type='submit' id='password-btn' class='orange-button small-btn' value='Save Password'></button>" +
         "</form>"));
     $('#tab-content').append(passwordContainer);
+
+    $('#password-btn').click(function (event) {
+        event.preventDefault();
+        console.log('Password save button clicked');
+    })
 });
 
+// notification tab event listener
 $('#notification-tab').click(function (event) {
     var notificationContainer = createContentContainer('notification-container', 'notification-header', 'Notification Settings', 'notification-sub', '');
 
@@ -118,79 +180,3 @@ $('#notification-tab').click(function (event) {
     $('#tab-content').append(notificationContainer);
 })
 
-$('#submit-btn').click(function (event) {
-    event.preventDefault();
-    console.log('Password save button clicked');
-})
-
-$('#edit-btn').click(function (event) {
-    event.preventDefault();
-    $('#profile-name-input').css({ 'background-color': 'white' });
-    $('#profile-email-input').css({ 'background-color': 'white' });
-    $('#profile-phone-input').css({ 'background-color': 'white' });
-    $('#profile-currentpassword-input').css({'background-color': 'white'});
-    $('#profile-newpassword-input').css({'background-color': 'white'});
-    $('#profile-confirmpassword-input').css({'background-color': 'white'});
-
-    $('#profile-name-input').removeAttr('readonly');
-    $('#profile-email-input').removeAttr('readonly');
-    $('#profile-phone-input').removeAttr('readonly');
-    $('#profile-currentpassword-input').removeAttr('readonly');
-    $('#profile-newpassword-input').removeAttr('readonly');
-    $('#profile-confirmpassword-input').removeAttr('readonly');
-
-    $('#edit-btn').css({ 'display': 'none' });
-    $('#save-btn').css({ 'display': 'block' });
-});
-
-
-$('#save-btn').click((event) => {
-    event.preventDefault();
-
-    $('#profile-name-input').css({ 'background-color': 'inherit' });
-    $('#profile-email-input').css({ 'background-color': 'inherit' });
-    $('#profile-phone-input').css({ 'background-color': 'inherit' });
-    $('#profile-currentpassword-input').css({'background-color': 'inherit'});
-    $('#profile-newpassword-input').css({'background-color': 'inherit'});
-    $('#profile-confirmpassword-input').css({'background-color': 'inherit'});
-
-    $('#profile-name-input').attr('readonly', 'true');
-    $('#profile-email-input').attr('readonly', 'true');
-    $('#profile-phone-input').attr('readonly', 'true');
-    $('#profile-currentpassword-input').attr('readonly', 'true');
-    $('#profile-newpassword-input').attr('readonly', 'true');
-    $('#profile-confirmpassword-input').attr('readonly', 'true');
-
-    $('#save-btn').css({ 'display': 'none' });
-    $('#edit-btn').css({ 'display': 'block' });
-
-
-    //POST
-    var name = $('#profile-name-input').val();
-    var phone = $('#profile-phone-input').val();
-    var email = $('#profile-email-input').val();
-    var newpassword = $('#profile-newpassword-input').val();
-    const dataToSend = {
-        name: name,
-        phone: phone,
-        email: email,
-        password: newpassword
-    }
-
-
-
-    fetch('/users/me', {
-        method: 'PATCH',
-        body: JSON.stringify(dataToSend),
-        headers: {
-            'content-type': 'application/json',
-            'Authorization': 'Bearer ' + jwt
-        }
-    }).then(res => {
-        console.log(res)
-
-    }).then((response) => {
-    }).catch(error => console.error('Error:', error));
-
-
-});
